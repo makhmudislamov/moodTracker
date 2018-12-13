dd321c44c518d9adbfbd2b7882b0ee7e9a481c6b
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-12-13 13:16:14 -0800

    1st commit

---
+++ Mood Tracker/MoodEntry.swift
@@ -0,0 +1,62 @@
+//
+//  MoodEntry.swift
+//  Mood Tracker
+//
+//  Created by Erick Sanchez on 8/14/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIColor
+
+struct MoodEntry {
+    
+    enum Mood: Int {
+        case none
+        case amazing
+        case good
+        case neutral
+        case bad
+        case terrible
+        
+        var stringValue: String {
+            switch self {
+            case .none:
+                return ""
+            case .amazing:
+                return "Amazing"
+            case .good:
+                return "Good"
+            case .neutral:
+                return "Neutral"
+            case .bad:
+                return "Bad"
+            case .terrible:
+                return "Terrible"
+            }
+        }
+        
+        var colorValue: UIColor {
+            switch self {
+            case .none:
+                return .clear
+            case .amazing:
+                return .green
+            case .good:
+                return .blue
+            case .neutral:
+                return .gray
+            case .bad:
+                return .orange
+            case .terrible:
+                return .red
+            }
+        }
+    }
+    
+    var mood: Mood
+    var date: Date
+    
+}
+
+// In order this file to be accesed by watchExtention file group click "WatchMoodTracker" on Target Membership
